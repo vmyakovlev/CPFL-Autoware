4d6474cb890bfa7570e3be4725e06175e5cd3e7d
Sumanth <sumanth.724@gmail.com>
2018-03-22 15:13:19 -0700

    removed the heuristic to switch to stopline state switch to stopline state is based on param_target_waypoint_

--- ros/src/computing/planning/decision/packages/decision_maker/nodes/decision_maker/decision_maker_node_callback.cpp
+++ ros/src/computing/planning/decision/packages/decision_maker/nodes/decision_maker/decision_maker_node_callback.cpp
@@ -395,25 +395,35 @@ void DecisionMakerNode::callbackFromFinalWaypoint(const autoware_msgs::lane &msg
   // cached
   current_finalwaypoints_ = msg;
 
-  static size_t previous_idx = 0;
-  
+//  static size_t previous_idx = 0;
+//
   size_t idx = param_stopline_target_waypoint_ +  (current_velocity_ * param_stopline_target_ratio_);
   idx = current_finalwaypoints_.waypoints.size() - 1 > idx ?
 		idx : current_finalwaypoints_.waypoints.size() - 1;
+//
+//  CurrentStoplineTarget_ = current_finalwaypoints_.waypoints.at(idx);
+    CurrentStoplineTarget_ = current_finalwaypoints_.waypoints.at(param_target_waypoint_);
 
-  CurrentStoplineTarget_ = current_finalwaypoints_.waypoints.at(idx);
-  
-  for(size_t i = (previous_idx>idx)?idx:previous_idx ; i <= idx; i++){
-	  if(i < current_finalwaypoints_.waypoints.size()){
-		  if (current_finalwaypoints_.waypoints.at(i).wpstate.stopline_state == autoware_msgs::WaypointState::TYPE_STOPLINE){
+    for(size_t i = 0 ; i <= current_finalwaypoints_.waypoints.size()-1; i++){
+        std::cerr << i <<":"<< ((current_finalwaypoints_.waypoints.at(i).wpstate.stopline_state == autoware_msgs::WaypointState::TYPE_STOPLINE)?"\033[1;31m1\033[0m":"0" )<< " " ;
+    }
+    std::cerr << "\n";
+
+  //for(size_t i = (previous_idx>idx)?idx:previous_idx ; i <= idx; i++){
+    for(size_t i = 0 ; i <= current_finalwaypoints_.waypoints.size()-1; i++){
+	  //if(i < current_finalwaypoints_.waypoints.size()){
+		  if (current_finalwaypoints_.waypoints.at(param_target_waypoint_).wpstate.stopline_state == autoware_msgs::WaypointState::TYPE_STOPLINE){
 			  ctx->setCurrentState(state_machine::DRIVE_ACC_STOPLINE_STATE);
 			  closest_stopline_waypoint_ = CurrentStoplineTarget_.gid;
+            std::cerr << "\033[1;31mDRIVE_ACC_STOPLINE_STATE\033[0m\n";
 		  }
-		  if (current_finalwaypoints_.waypoints.at(i).wpstate.stopline_state == autoware_msgs::WaypointState::TYPE_STOP)
-			  ctx->setCurrentState(state_machine::DRIVE_ACC_STOP_STATE);
-	  }
+		  if (current_finalwaypoints_.waypoints.at(param_target_waypoint_).wpstate.stopline_state == autoware_msgs::WaypointState::TYPE_STOP) {
+            ctx->setCurrentState(state_machine::DRIVE_ACC_STOP_STATE);
+            std::cerr << "\033[1;31mDRIVE_ACC_STOP_STATE\033[0m\n";
+          }
+	 // }
   }
-  previous_idx = idx;
+//  previous_idx = idx;
 
   // steering
   idx = current_finalwaypoints_.waypoints.size() - 1 > param_target_waypoint_ ?
